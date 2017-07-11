d5a4bcf088e909873cce56cec48ad55dfb607d0a
subhannizar25 <subhannizar25@gmail.com>
2017-07-11 09:22:32 +0800

    add vscode to gitignore

--- websen/__init__.py
+++ websen/__init__.py
@@ -7,7 +7,7 @@ UPLOAD_FOLDER = os.path.sep + 'uploads' + os.path.sep+"profile"+os.path.sep
 DATA_FOLDER = os.path.sep + 'temp' + os.path.sep
 app.config['UPLOAD_FOLDER'] = app.root_path +os.path.sep+"static"+ UPLOAD_FOLDER
 app.config['DATA_FOLDER'] = app.root_path + DATA_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:keyfaton@localhost/websen_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:b1sm1llah@localhost/websen_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.debug = True
 
